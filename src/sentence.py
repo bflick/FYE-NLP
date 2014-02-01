cf63ed82d6986cb7ff68848bfad28ad8a1ac5cad
Brian Flick <flickb@email.sc.edu>
2014-01-31 19:58:18 -0500

    word class added but not functional

--- src/sentence.py
+++ src/sentence.py
@@ -4,32 +4,41 @@ class sentence( object ):
 
 	@property
 	def words( self ):
-		return self.words
+		return self.wordList
 
 	@property
 	def taggedWords( self ):
-		return self.taggedWords
+		return self._taggedWords
 
 	@property
 	def rawText( self ):
+		rawText = ''
 		rt_ind = 0
 		punctuation = set('.,?:;"\'`!')
 		for tkn in self.words:
-			if any((c in tkn) for c in puntutation) or rt_ind == 0:
-				rawText = tkn
+			if any((c in tkn) for c in punctuation) or rt_ind == 0:
+				rawText += tkn
 			else:
 				rawText += ' ' + tkn
+			rt_ind += 1
 		return rawText
 
 	def __init__( self, listOfTokens ):
-		self.words = listOfTokens
+		self.wordList = listOfTokens
 		self.index = 0
-		self.taggedWords = []
+		self._taggedWords = []
 		tgdWords = pos_tag( listOfTokens )
 		for w in tgdWords:
-			self.taggedWords.append( w )
+			self._taggedWords.append( w )
 
 	def __iter__( self ):
 		return self
 
 	def next( self ):
+		ret = ''
+		if self.index == len(self.words):
+			raise StopIteration
+
+		ret = self.wordList[self.index]
+		self.index += 1
+		return ret
